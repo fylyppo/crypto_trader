08e12a74ad15cdfacbf546d7e2537f5d03ba4ae6
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-06-16 15:40:32 +0200

    Update SignInPage

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -1,10 +1,49 @@
+import 'package:another_flushbar/flushbar_helper.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:crypto_trader/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart';
+import 'package:crypto_trader/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 
-class SignInPage extends StatelessWidget {
-  const SignInPage({Key? key}) : super(key: key);
+import 'widgets/sign_in_form.dart';
+
+class SignInPage extends StatelessWidget implements AutoRouteWrapper {
+  final void Function() onSuccess;
+  const SignInPage({
+    Key? key,
+    required this.onSuccess,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: BlocListener<SignInFormBloc, SignInFormState>(
+        listener: (context, state) {
+          if (state.status == FormzStatus.submissionSuccess) {
+            return onSuccess();
+          } else if (state.failure != null) {
+            FlushbarHelper.createError(
+              message: state.failure!.map(
+                cancelledByUser: (_) => 'Cancelled',
+                serverError: (_) => 'Server error',
+                emailAlreadyInUse: (_) => 'Email already in use',
+                invalidEmailAndPasswordCombination: (_) =>
+                    'Invalid email and password combination',
+              ),
+            ).show(context);
+          }
+        },
+        child: const SignInForm(),
+      ),
+    );
+  }
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<SignInFormBloc>(),
+      child: this,
+    );
   }
 }
