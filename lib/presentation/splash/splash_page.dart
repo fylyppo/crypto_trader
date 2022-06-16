d32fd6640b026220804d654bc964994cf5a30f45
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-06-16 15:38:31 +0200

    Navigate from SplashPage

--- lib/presentation/splash/splash_page.dart
+++ lib/presentation/splash/splash_page.dart
@@ -1,23 +1,35 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../application/auth/bloc/auth_bloc.dart';
 import '../../injection.dart';
 import '../routes/app_router.gr.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatelessWidget implements AutoRouteWrapper {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+      child: this,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    getIt<AppRouter>().replaceNamed('/home-page');
     return BlocListener<AuthBloc, AuthState>(
       listener: (context, state) {
-        state.mapOrNull(
-            authenticated: (_) => getIt<AppRouter>().replaceNamed('/home-page'),
-            unauthenticated: (_) => getIt<AppRouter>().replaceNamed('/sign-in-page'));
+        state.maybeMap(
+          initial: null,
+          orElse: () => getIt<AppRouter>().replaceNamed('/home-page'));
       },
-      child: const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      child: const SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       ),
     );
