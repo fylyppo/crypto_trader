2e7f98498975cf655967d3fb9074ebe7c2babb8f
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-07-03 10:50:09 +0200

    Delete and update enums in Order

--- lib/domain/spot/order.dart
+++ lib/domain/spot/order.dart
@@ -1,37 +1,27 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'order.freezed.dart';
 
 enum OrderSide {
-  @JsonValue('BUY')
   buy,
-  @JsonValue('SELL')
   sell,
 }
 
 enum OrderType {
-  @JsonValue('LIMIT')
   limit,
-  @JsonValue('MARKET')
   market,
-  @JsonValue('STOP_LOSS')
-  stopLoss,
-  @JsonValue('STOP_LOSS_LIMIT')
-  stopLossLimit,
-  @JsonValue('TAKE_PROFIT')
-  takeProfit,
-  @JsonValue('TAKE_PROFIT_LIMIT')
-  takeProfitLimit,
-  @JsonValue('LIMIT_MAKER')
-  limitMaker,
+  stop_loss,
+  stop_loss_limit,
+  take_profit,
+  take_profit_limit,
+  limit_maker,
 }
 
 enum TimeInForce {
-  @JsonValue('GTC')
   gtc,
-  @JsonValue('IOC')
   ioc,
-  @JsonValue('FOK')
   fok,
 }
 
